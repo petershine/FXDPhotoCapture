7cf3c6ae403286eb4dc1e15c8efdcdb0de24f116
petershine <petershine@fxceed.com>
2012-03-07 17:58:27 +0900

    Initial Commit

---
+++ FXDPhotoCapture/FXDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FXDAppDelegate.h
+//  FXDPhotoCapture
+//
+//  Created by Peter SHINe on 3/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FXDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FXDPhotoCapture/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FXDPhotoCapture
+//
+//  Created by Peter SHINe on 3/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FXDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([FXDAppDelegate class]));
+	}
+}
